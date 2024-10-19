a2ee4e31258ce177ce1eec166c22e622b9d50615
geo-ant <54497890+geo-ant@users.noreply.github.com>
2024-10-19 10:41:28 +0200

    allows placing arguments paragra[h now

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use quote::quote;
+use quote::{quote, ToTokens};
 use syn::{parse_macro_input, Attribute, FnArg, Ident, ItemFn, Pat};
-use util::{extract_doc_attrs, prepend_to_doc_attribute};
+use util::{extract_doc_attrs, extract_fn_doc_attrs, prepend_to_doc_attribute};
 mod util;
 
 #[proc_macro_attribute]
@@ -20,7 +20,12 @@ pub fn doxidize(
     }
 
     // extrac the doc attributes on the function itself
-    let function_docs = extract_doc_attrs(&mut function.attrs);
+    let function_docs = match extract_fn_doc_attrs(&mut function.attrs) {
+        Ok(docs) => docs,
+        Err(err) => {
+            return err.into_compile_error().into();
+        }
+    };
     // extract the doc attributes on the parameters
     for arg in function.sig.inputs.iter_mut() {
         match arg {
@@ -67,38 +72,54 @@ pub fn doxidize(
         }
     });
 
+    let docs_before = function_docs.before_args_section;
+    let docs_after = function_docs.after_args_section;
+    let maybe_empty_doc_line = if !docs_after.is_empty() {
+        Some(quote! {#[doc=""]})
+    } else {
+        None
+    };
+
     quote! {
-        #(#function_docs)*
+        #(#docs_before)*
         #[doc=""]
         #[doc=" # Arguments"]
         #[doc=""]
         #(#parameter_doc_blocks)*
+        #maybe_empty_doc_line
+        #(#docs_after)*
         #function
     }
     .into()
 }
 
-// helper attribute
-//@todo document
+// this is to expose the helper attribute #[arguments_section].
+// The only logic about this attribute that this here function includes is
+// to make sure that this attribute is not placed before the #[doxidize]
+// attribute. All other logic is handled in the doxidize macro itself.
 #[proc_macro_attribute]
-pub fn arguments(
-    attr: proc_macro::TokenStream,
+pub fn arguments_section(
+    _attr: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    let mut function: ItemFn = parse_macro_input!(item as ItemFn);
+    let function: ItemFn = parse_macro_input!(item as ItemFn);
 
     // enforce that this macro comes after doxidize, which means it
     // cannot see the doxidize attribute
-    let result = function.attrs.into_iter().try_fold((),|cum, attr| {
-        cum?;
-       if attr.path().is_ident("doxidize") {
-           Err(syn::Error::new_spanned(&attr.path(),"The #[doxidize] macro must come before this attribute. Place it on top of the doc comments of the function."))
-        } else {
-            Ok(())
-        }
-    }); 
+    let maybe_doxidize = function
+        .attrs
+        .iter()
+        .find(|attr| is_arguments_section(attr));
+    if let Some(attr) = maybe_doxidize {
+        syn::Error::new_spanned(&attr,"The #[doxidize] attribute must come before the arguments section attribute.\nPlace it on top of the doc comments of the function.").into_compile_error().into()
+    } else {
+        function.to_token_stream().into()
+    }
+}
 
-    todo!("make sure this attribute comes after doxidize")
-    //@todo and also make sure that it does not occur multiple times
-    item
+/// check whether an attribute is the arguments section attribute.
+/// Stick this into it's own function so I can change the logic
+#[inline(always)]
+fn is_arguments_section(attr: &Attribute) -> bool {
+    attr.path().is_ident("arguments_section")
 }
--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,8 @@
 use quote::quote;
 use syn::{Attribute, Expr, LitStr, Meta, MetaNameValue};
 
+use crate::is_arguments_section;
+
 /// Function to prepend a string to a `#[doc]` attribute.
 pub fn prepend_to_doc_attribute(prepend_text: &str, attr: &Attribute) -> proc_macro2::TokenStream {
     // Parse the attribute to see if it's a MetaNameValue (e.g., #[doc = "..."])
@@ -46,4 +48,54 @@ pub fn extract_doc_attrs(attrs: &mut Vec<Attribute>) -> Vec<Attribute> {
         .collect();
     attrs.retain(|attr| !attr.path().is_ident("doc"));
     doc_attrs
+}
+
+/// function doc attributes split by whether they occur above or below
+/// the attribute that indicates where the argument section should be placed.
+/// If no such attribute is present, we stick all the doc attributes in the
+/// before section
+pub struct FunctionDocs {
+    pub before_args_section: Vec<Attribute>,
+    pub after_args_section: Vec<Attribute>,
+}
+
+pub fn extract_fn_doc_attrs(attrs: &mut Vec<Attribute>) -> Result<FunctionDocs, syn::Error> {
+    let mut before_args_section = Vec::with_capacity(attrs.len());
+    let mut after_args_section = Vec::with_capacity(attrs.len());
+
+    // I'm sure this could be done with iterators...
+    // I'm just too dumb to do that now
+    let mut idx = 0;
+    // parse the arguments before the arguments-section attribute
+    while idx < attrs.len() {
+        let current_attr = attrs.get(idx).unwrap();
+        if is_arguments_section(current_attr) {
+            break;
+        }
+        if current_attr.path().is_ident("doc") {
+            before_args_section.push(current_attr.clone());
+        }
+        idx += 1;
+    }
+
+    while idx < attrs.len() {
+        let current_attr = attrs.get(idx).unwrap();
+        if is_arguments_section(current_attr) {
+            return Err(syn::Error::new_spanned(
+                current_attr,
+                "This attribute must only occur once",
+            ));
+        }
+        if current_attr.path().is_ident("doc") {
+            after_args_section.push(current_attr.clone());
+        }
+        idx += 1;
+    }
+
+    // delete all doc attributes from the function (and the arguments section attributes that I don't need anymore)
+    attrs.retain(|attr| !attr.path().is_ident("doc") && !is_arguments_section(attr));
+    Ok(FunctionDocs {
+        before_args_section,
+        after_args_section,
+    })
 }
