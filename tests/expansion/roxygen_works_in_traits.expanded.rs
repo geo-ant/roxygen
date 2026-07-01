06720fc807296af13d41a92e649d4ec83cf39211
geo-ant <54497890+geo-ant@users.noreply.github.com>
2026-07-01 22:23:13 +0200

    all code and no play makes geo a dull boy

--- tests/expansion/roxygen_works_in_traits.expanded.rs
+++ tests/expansion/roxygen_works_in_traits.expanded.rs
@@ -18,14 +18,13 @@ trait Foo {
     /// **Parameters**:
     ///
     /// * `first`: the first parameter
-    /// * `second`: the second, more interesting parameter
-    ///    with more interesting
+    /// * `second`:    with more interesting
     ///
-    ///    ```rust
-    ///      docs = very_interesting!();
-    ///    ```
+    ///       ```rust
+    ///         docs = very_interesting!();
+    ///       ```
     ///
-    ///    Sorry it's kinda late and I'm tired... but my professional
-    ///    honor is not letting me skip this test.
+    ///       Sorry it's kinda late and I'm tired... but my professional
+    ///       honor is not letting me skip this test.
     fn bar(first: i32, second: f32);
 }
