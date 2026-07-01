ede5e5db616a3c8e4066d988f13fdf8fbcf7f406
geo-ant <54497890+geo-ant@users.noreply.github.com>
2026-07-01 22:18:22 +0200

    add a test for the failure that the user actually reported. Still works, though

--- tests/expansion/roxygen_works_in_traits.expanded.rs
+++ tests/expansion/roxygen_works_in_traits.expanded.rs
@@ -11,4 +11,21 @@ trait Foo {
     fn foo(bar: u32, baz: String, _undocumented: i32) -> bool {
         baz.len() > bar as usize
     }
+    /// I almost forgot the whole point was to test
+    /// whether it works for functions without body...
+    /// it should...
+    ///
+    /// **Parameters**:
+    ///
+    /// * `first`: the first parameter
+    /// * `second`: the second, more interesting parameter
+    ///    with more interesting
+    ///
+    ///    ```rust
+    ///      docs = very_interesting!();
+    ///    ```
+    ///
+    ///    Sorry it's kinda late and I'm tired... but my professional
+    ///    honor is not letting me skip this test.
+    fn bar(first: i32, second: f32);
 }
